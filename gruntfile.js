30a3405448d3af37fc5ba4f736377b0f80da90eb
Dale Sande <dale.sande@gmail.com>
2013-11-29 15:12:25 -0800

    update output css name and gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'public/stylesheets/style.css' : 'sass/application.scss'
+          'public/stylesheets/application.css' : 'sass/application.scss'
         }
       }
     },
