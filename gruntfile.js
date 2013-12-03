84bfd6cfc54369a810d0809ea493503de25d69e3
Dale Sande <dale.sande@gmail.com>
2013-12-03 11:14:51 -0800

    add support for livereload

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,10 @@ module.exports = function(grunt) {
     watch: {
       source: {
         files: ['sass/**/*.scss'],
-        tasks: ['sass']
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        }
       }
     }
   });
